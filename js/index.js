d249274d9e94ee13031d5a4a004c82f05a5d12cc
Kat Tochowicz <ktochow1@binghamton.edu>
2019-12-18 16:12:35 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
 let k = 0;
-//this is a change 4:03 wednesday
+//this is a change 4:03 wednesdaychangechange
